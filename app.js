a52067c3372869aa7040f7606b583e04ada22819
Eric Butler <ericbutler555@gmail.com>
2018-07-08 23:38:07 -0400

    Added campground show route and template

--- app.js
+++ app.js
@@ -20,21 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set up the campground database schema:
 var campgroundSchema = mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Try adding one to the db (temporary, for testing only):
-// Campground.create({
-//   name: "Salmon Creek",
-//   image: "https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104496f1c17ca4ebb6b9_340.jpg"
-// }, function(error, response){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('item added: ' + response);
-//   }
-// });
 
 // routes:
 
@@ -42,23 +31,16 @@ app.get('/', function(req, res){
   res.render('home.ejs');
 });
 
-// (Removing this as we move the data into the database)
-// var campgrounds = [
-//   { name: "Salmon Creek", image: "https://pixabay.com/get/e834b70c2cf5083ed1584d05fb1d4e97e07ee3d21cac104496f1c17ca4ebb6b9_340.jpg" },
-//   { name: "Granite Hill", image: "https://pixabay.com/get/ea31b10929f7063ed1584d05fb1d4e97e07ee3d21cac104496f1c17ca4ebb6b9_340.jpg" },
-//   { name: "Mountain Goat’s Rest", image: "https://pixabay.com/get/e136b60d2af51c22d2524518b7444795ea76e5d004b0144294f0c47aa1eeb4_340.jpg" }
-// ];
-
 app.get('/campgrounds', function(req, res){
   // get all campgrounds from db:
-  Campground.find({}, function(error, response){
+  Campground.find({}, function(error, data){
     if (error) {
       console.log(error);
       res.redirect('/');
     } else {
       // render the template, with "campgrounds" now populated by the returned db data:
       res.render('campgrounds.ejs', {
-        campgrounds: response
+        campgrounds: data
       });
     }
   });
@@ -68,13 +50,26 @@ app.post('/campgrounds', function(req, res){
   Campground.create({
     name: req.body.campName,
     image: req.body.campImage
-  }, function(error, response){
+  }, function(error, data){
     if (error) {
       console.log(error);
       res.redirect('/campgrounds');
     } else {
-      console.log('New campground added: ' + response);
+      console.log('New campground added: ' + data);
       res.redirect('/campgrounds');
+    }
+  });
+});
+
+app.get('/campgrounds/:id', function(req, res){
+  Campground.findById(req.params.id, function(error, data){
+    if (error) {
+      console.log(error);
+      res.redirect('/campgrounds');
+    } else {
+      res.render('campground.ejs', {
+        campground: data
+      });
     }
   });
 });
