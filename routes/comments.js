8ccfbc9c5b2359d136cbaca15f65325ff58a697f
Eric Butler <ericbutler555@gmail.com>
2018-07-22 03:40:36 -0400

    Added edit/delete functionality for comments, with authorship check

--- routes/comments.js
+++ routes/comments.js
@@ -13,6 +13,28 @@ function isLoggedIn(req, res, next) {
   }
 }
 
+// define authorship middleware:
+function isCommentAuthor(req, res, next) {
+  if (req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, data){
+      if (err) {
+        console.log(err);
+        res.redirect('back');
+      } else {
+        if (data.author.id && data.author.id.equals(req.user._id)){
+          return next();
+        } else {
+          console.log('You are not the author of this entry');
+          res.redirect('back');
+        }
+      }
+    });
+  } else {
+    console.log('You must be logged in to do this');
+    res.redirect('back');
+  }
+}
+
 // comment new (must be logged-in to access)
 router.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, data){
@@ -49,5 +71,45 @@ router.post('/campgrounds/:id/comments', isLoggedIn, function(req, res){
     res.redirect('/campgrounds/' + req.params.id);
   });
 });
+
+// comment edit
+router.get('/campgrounds/:id/comments/:comment_id/edit', isCommentAuthor, function(req, res){
+  Comment.findById(req.params.comment_id, function(err, data){
+    if (err) {
+      console.log(err);
+      res.redirect('back');
+    } else {
+      res.render('comments/edit.ejs', {
+        campgroundId: req.params.id,
+        comment: data
+      });
+    } // end else
+  });
+});
+
+// comment update
+router.put('/campgrounds/:id/comments/:comment_id', isCommentAuthor, function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, data){
+    if (err) {
+      console.log(err);
+      res.redirect('back');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
+// comment delete
+router.delete('/campgrounds/:id/comments/:comment_id', isCommentAuthor, function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Comment deleted');
+    }
+    res.redirect('/campgrounds/' + req.params.id);
+  });
+});
+
 
 module.exports = router;
